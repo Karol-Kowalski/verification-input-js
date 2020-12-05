4053ebab158470ab4ed3b06b12490468be3cbceb
Virion <kajko195@tlen.pl>
2020-12-05 23:58:16 +0100

    Select field when it's filled
    Now when user fill fields proper only writing next code's character.
    When the field is not empty text insight highlight.

---
+++ index.js
@@ -0,0 +1,25 @@
+const form = document.querySelector('[name="Verify"]')
+const inputs = document.querySelectorAll('.inputs input')
+
+function handleInput(e) {
+    const input = e.target
+
+    if(input.nextElementSibling && input.value) {
+        input.nextElementSibling.focus()
+        input.nextElementSibling.select()
+    }
+}
+
+function handlePaste(e) {
+    const paste = e.clipboardData.getData('text')
+
+    inputs.forEach((input, i) => {
+        input.value = paste[i]
+    })
+}
+
+
+form.addEventListener('input', handleInput)
+inputs[0].addEventListener('paste', handlePaste)
+
+// 123456
